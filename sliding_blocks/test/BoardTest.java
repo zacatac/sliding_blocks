c64d72c9edc3f3df69efcce45ba7c278e8683f10
zacatac <field.zackery@gmail.com>
2013-08-05 10:03:59 -0700

    Maybe this time the test src folder will show up

---
+++ sliding_blocks/test/BoardTest.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class BoardTest {
+
+    @Test
+    public void testBoard(){
+        Board board = new Board("5 4");
+        Block b1 = new Block("0 0 1 0");
+        Block b2 = new Block("0 3 1 3");
+        Block b3 = new Block("2 0 3 0");
+        Block b4 = new Block("2 3 3 3");
+        Block b5 = new Block("1 1 2 2");
+        Block b6 = new Block("3 1 3 2");
+        Block b7 = new Block("4 0 4 0");
+        Block b8 = new Block("4 1 4 1");
+        Block b9 = new Block("4 2 4 2");
+        Block b10 = new Block("4 3 4 3");
+
+        board.addBlock(b1);
+        board.addBlock(b2);
+        board.addBlock(b3);
+        board.addBlock(b4);
+        board.addBlock(b5);
+        board.addBlock(b6);
+        board.addBlock(b7);
+        board.addBlock(b8);
+        board.addBlock(b9);
+        board.addBlock(b10);
+        board.print();
+
+    }
+
+}
