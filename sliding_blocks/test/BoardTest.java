ea502a299c54144f8e7075222384ebdb5601ce59
zacatac <field.zackery@gmail.com>
2013-08-12 10:11:37 -0700

    BoardTest with tests for changeWhiteSpaces

--- sliding_blocks/test/BoardTest.java
+++ sliding_blocks/test/BoardTest.java
@@ -9,149 +9,67 @@ import junit.framework.TestCase;
 
 public class BoardTest extends TestCase{
 
-	public void testGetNotEmpty(){
-		//test a completely false notEmpty array
-		Board b = new Board(5,5);
-		boolean[][] test = new boolean[5][5];
-		
-		for (int i = 0; i< 5; i++){
-			for (int j = 0; j <5; j++){
-				assertFalse(b.getNotEmpty()[i][j]);
-			}
-		}
-		
-		//test adding a 1x2 block located at topleft of 0,0 
-		Board b2 = new Board(4,4);
-		
-		int[] array = {0,0};
-		b2.addBlock("1 2", array, true);
-		
-		for (int i = 0; i< 1; i++){
-			for (int j = 0; j <2; j++){
-				assertTrue(b2.getNotEmpty()[i][j]);
-			}
-		}
-		
-		//test adding a 2x2 block covering the whole board
-//WHY DOES THIS NOT WORK D:
-		Board b3 = new Board(2,2);
-		b3.addBlock("2 2", array, true);
-		assertTrue(b2.getNotEmpty()[0][0]);
-		assertTrue(b2.getNotEmpty()[0][1]);
-		assertTrue(b2.getNotEmpty()[1][0]);
-		assertTrue(b2.getNotEmpty()[1][1]);
-//SAME THING, DIFFERENT FORMAT
-		for (int i = 0; i< 2; i++){
-			for (int j = 0; j <2; j++){
-				assertTrue(b2.getNotEmpty()[i][j]);
-			}
-		}
-		
-	}
-	
-	public void testAddBlock(){
-		Board board1 = new Board(4,4);
-		Board board2 = new Board(4,4);
-		System.out.println("HELP");
+    public void testKeyToIntArray(){
+        Board board = new Board(2,2);
+        String s4 = "1 2";
+        String s1 = "2 3";
+        String s2 = "234 456";
+        String s3 = "1 2";
 
-		int[] array1 = {1,0};
-		int[] array2 = {2,1};
-		int[] array3 = {3,2};
 
-		 
-		board1.addBlock("1 1",array1, true);
-		board1.addBlock("1 1", array2, true);
-		board1.addBlock("1 2", array3, true);
-		
-	    //test adding block onto existing block
-		int[] array4 = {1,0};
+        int[] i4 = board.keyToIntArray(s4);
+        int[] i1 = board.keyToIntArray(s1);
+        int[] i2 = board.keyToIntArray(s2);
+        int[] i3 = board.keyToIntArray(s3);
 
-		board2.addBlock("1 1",array4, true);
-		try {
-			board2.addBlock("1 1", array4, true);
-			fail("Did Not Catch Illegal Argument Exception");
-		} catch (IllegalArgumentException e) {}
-		
+        int[] exp4 = {1,2};
+        int[] exp1 = {2,3};
+        int[] exp2 = {234,456};
+        int[] exp3 = {1,2};
 
-		//testing adding block outside of board
-		Board board3 = new Board(4,4);		
-		int[] array5 = {10,10};
-		try{
-			board3.addBlock("2 2", array5, true);
-			fail("not supposed to see this");
-		}
-		catch (IllegalArgumentException e ){
-		}
-	}
-	public void testRemoveBlock() throws IllegalArgumentException{
-		System.out.println("~~~Remove block test~~~");
-		Board board4 = new Board(4,4);
-		int[] array8 = {0,2};
-		int[] array9 = {1,1};
-		int[] array10 = {6,6};
+        assertArrayEquals(exp4,i4);
+        assertArrayEquals(exp1,i1);
+        assertArrayEquals(exp2,i2);
+        assertArrayEquals(exp3,i3);
 
-		board4.addBlock("11", array8, true);
-		board4.addBlock("11", array9, true);
-		board4.removeBlock("11", array9, true);
-		System.out.println(board4);
-		
-		//Remove block test: no block to remove
-		Board board5 = new Board(4,4);
-		try{
-			board5.removeBlock("11", array8, true);
-			fail("Couldn't catch Illegal Argument Ex for removing non-existing block");
-		}catch (IllegalArgumentException e) {}
-		
-		
-		//Remove block test: out of bounds
-		Board board6 = new Board(4,4);
-		try {
-			board6.removeBlock("11", array10, true);
-			fail("Can't catch");
-		} catch (ArrayIndexOutOfBoundsException e) {
-		}
-		System.out.println(board6);
-	}
-	
-	public void testFindMoves(){
-		Board b = new Board(4,4);
-		int[] array1 = {1,0};
-		int[] array2 = {2,1};
-		int[] array3 = {3,2};
-		
-		b.addBlock("11",array1, true);
-		b.addBlock("11",array2, true);
-		b.addBlock("12", array3, true);
-		System.out.println("TEST FIND MOVES / not working");
-		System.out.println(b.findMoves(2));
-	}
-	
-	
-	//These tests are
-	public void testChangeWhiteSpaces(){
-		System.out.println( "~~~AddBlock Test, change white spaces~~~");
-		Board board7 = new Board(4,4);
-	}
-	
-	public void testThisMoveWorked(){
-		System.out.println("~~~test this move worked");
-		Board board = new Board(4,4);
-		
-	}
-	
-	public void testIsFreeToMove(){
-		System.out.println("Testing isFreeToMove");
-		Board board = new Board(5,4);
-		
-		
-	}
+    }
 
-	public void testKeyToIntArray(){
-		System.out.println("~~~testing key to int array");
-		Board board = new Board(5,4);
-		int[] array1 = {1,0};
-		int[] array2 = {2,1};
-		int[] array3 = {3,2};
-	}
-	
+    public void testChangeWhiteSpaces(){
+        Board board = new Board(3,3);
+
+        int[] upperLeft = {0,0};
+        board.changeWhiteSpaces("1 1",upperLeft,true);
+
+        boolean[][] expected1 = {{true,false,false},
+                                {false,false,false},
+                                {false,false,false}};
+
+        assertArrayEquals(expected1,board.getNotEmpty());
+
+        board.changeWhiteSpaces("1 1",upperLeft,false);
+
+        boolean[][] expected2 = {{false,false,false},
+                {false,false,false},
+                {false,false,false}};
+
+        assertArrayEquals(expected2,board.getNotEmpty());
+
+
+        board.changeWhiteSpaces("3 3",upperLeft,true);
+
+        boolean[][] expected3 = {{true,true,true},
+                {true,true,true},
+                {true,true,true}};
+
+        assertArrayEquals(expected3,board.getNotEmpty());
+
+
+        board.changeWhiteSpaces("3 3",upperLeft,false);
+
+        assertArrayEquals(expected2,board.getNotEmpty());
+
+
+    }
+
+
 }
